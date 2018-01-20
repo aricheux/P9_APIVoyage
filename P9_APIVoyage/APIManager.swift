7ea5befa52b22333210b1d7a8cb931ecd785742f
Richeux Antoine <richeux.antoin44@gmail.com>
2018-01-20 10:03:20 +0100

    Ajout d'une classe APIManager pour les requêtes API

---
+++ P9_APIVoyage/APIManager.swift
@@ -0,0 +1,14 @@
+//
+//  APIManager.swift
+//  P9_APIVoyage
+//
+//  Created by RICHEUX Antoine on 20/01/2018.
+//  Copyright © 2018 Richeux Antoine. All rights reserved.
+//
+
+import Foundation
+
+class APIManager {
+    static let sharedInstance = APIManager()
+    
+}
