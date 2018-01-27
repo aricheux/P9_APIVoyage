fb646c282ebe8f6923cf3720daad3febd06b0b8f
Richeux Antoine <richeux.antoin44@gmail.com>
2018-01-27 19:25:06 +0100

    création du uitableView pour afficher la météo des deux villes création d'une classe uitableViewCell pour modifier le texte et l'image du storyboard

--- P9_APIVoyage/WeatherPage/Controllers/WeatherController.swift
+++ P9_APIVoyage/WeatherPage/Controllers/WeatherController.swift
@@ -11,21 +11,37 @@ import Alamofire
 import SwiftyJSON
 
 class WeatherController: UITableViewController {
-
+    
+    var weatherResult = JSON()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let queryString = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text in (\"nantes\",\"ny\")) and u=\"c\""
 
         APIManager.sharedInstance.getWeather(with: queryString) { (jsonDict) in
-            let result = jsonDict["query"]["results"]["channel"]
-            print(result)
+            self.weatherResult = jsonDict["query"]["results"]["channel"]
+            print(self.weatherResult)
+            self.tableView.reloadData()
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weatherResult.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? WeatherTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of WeatherTableViewCell.")
+        }
+        
+        cell.cellLabel.text = weatherResult[indexPath.row]["astronomy"]["sunrise"].string
+        
+        return cell
     }
 
 }
---
+++ P9_APIVoyage/WeatherPage/Controllers/WeatherTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherTableViewCell.swift
+//  P9_APIVoyage
+//
+//  Created by RICHEUX Antoine on 27/01/2018.
+//  Copyright © 2018 Richeux Antoine. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+    @IBOutlet weak var cellImgView: UIImageView!
+    @IBOutlet weak var cellLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
